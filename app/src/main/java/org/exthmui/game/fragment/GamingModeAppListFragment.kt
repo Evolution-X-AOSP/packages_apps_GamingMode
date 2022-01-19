8056c33e206eef2df256b7ed70c73f89eedb4b0d
Pranav Vashi <neobuddy89@gmail.com>
2022-01-19 22:42:50 +0530

    Adapt for Evolution X

--- app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
+++ app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
@@ -17,8 +17,10 @@
 package org.exthmui.game.fragment
 
 import android.provider.Settings
+
 import androidx.preference.PreferenceDataStore
-import com.krypton.settings.preference.SystemSettingsStore
+
+import com.evolution.settings.preference.SystemSettingsStore
 
 import org.exthmui.game.R
 
